f52a085736d5d34c36680b2b4a848a18b75e7bb5
Eoin Flanagan <flanagep@tcd.ie>
2021-04-02 21:25:29 +0100

    Readded disabled tests

--- tests/BoundRayCast/updateLight.cpp
+++ tests/BoundRayCast/updateLight.cpp
@@ -11,34 +11,34 @@ class BoundRayCast_UpdateLightTest : public BaseLightingTest
         return new LightmapManager(TILE_SIZE, CastingAlgorithm::BOUND_RAY_CAST, 100);
     }
 };
-//
-//TEST_F(BoundRayCast_UpdateLightTest, update_light_location)
-//{
-//    int LIGHT_X = 22;
-//    int LIGHT_Y = 10;
-//    int UPDATED_LIGHT_X = 40;
-//    int UPDATED_LIGHT_Y = 30;
-//    int lightId = lightmapManager->addLight((LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI * 3, 10 * TILE_SIZE);
-//    lightmapManager->update();
-//    lightmapManager->updateLight(lightId, UPDATED_LIGHT_X * TILE_SIZE, UPDATED_LIGHT_Y * TILE_SIZE, PI * 3);
-//    lightmapManager->update();
-//
-//    EXPECT_TRUE(isTileUnlit(LIGHT_X, LIGHT_Y));
-//    EXPECT_TRUE(isTileLit(UPDATED_LIGHT_X, UPDATED_LIGHT_Y));
-//}
-//
-//TEST_F(BoundRayCast_UpdateLightTest, update_light_direction)
-//{
-//    int LIGHT_X = 22;
-//    int LIGHT_Y = 10;
-//    int lightId = lightmapManager->addLight((LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI, 10 * TILE_SIZE);
-//    lightmapManager->update();
-//    lightmapManager->updateLight(lightId, (LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI, glm::vec2(-1, 0));
-//    lightmapManager->update();
-//
-//    EXPECT_TRUE(isTileUnlit(LIGHT_X + 1, LIGHT_Y));
-//    EXPECT_TRUE(isTileLit(LIGHT_X - 1, LIGHT_Y));
-//}
+
+TEST_F(BoundRayCast_UpdateLightTest, update_light_location)
+{
+    int LIGHT_X = 22;
+    int LIGHT_Y = 10;
+    int UPDATED_LIGHT_X = 40;
+    int UPDATED_LIGHT_Y = 30;
+    int lightId = lightmapManager->addLight((LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI * 3, 10 * TILE_SIZE);
+    lightmapManager->update();
+    lightmapManager->updateLight(lightId, UPDATED_LIGHT_X * TILE_SIZE, UPDATED_LIGHT_Y * TILE_SIZE, PI * 3);
+    lightmapManager->update();
+
+    EXPECT_TRUE(isTileUnlit(LIGHT_X, LIGHT_Y));
+    EXPECT_TRUE(isTileLit(UPDATED_LIGHT_X, UPDATED_LIGHT_Y));
+}
+
+TEST_F(BoundRayCast_UpdateLightTest, update_light_direction)
+{
+    int LIGHT_X = 22;
+    int LIGHT_Y = 10;
+    int lightId = lightmapManager->addLight((LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI, 10 * TILE_SIZE);
+    lightmapManager->update();
+    lightmapManager->updateLight(lightId, (LIGHT_X + 0.5f) * TILE_SIZE, (LIGHT_Y + 0.5f) * TILE_SIZE, PI, glm::vec2(-1, 0));
+    lightmapManager->update();
+
+    EXPECT_TRUE(isTileUnlit(LIGHT_X + 1, LIGHT_Y));
+    EXPECT_TRUE(isTileLit(LIGHT_X - 1, LIGHT_Y));
+}
 
 TEST_F(BoundRayCast_UpdateLightTest, update_light_span)
 {
