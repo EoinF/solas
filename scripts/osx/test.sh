4be7bd56f8cf7cee5c267093802842491a58ead5
Eoin Flanagan <flanagep@tcd.ie>
2021-04-10 14:06:49 +0100

    osx test script now only builds the selected target instead of all targets

--- scripts/osx/test.sh
+++ scripts/osx/test.sh
@@ -3,8 +3,7 @@ mkdir -p build/test
 
 TARGET="${1:-allTests}"
 pushd build/test
-    cmake ../.. "-DBUILD_SCENARIO_VIEWER=OFF" "-DBUILD_TESTING=ON" "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake"
-    cmake --build .
-    make
+    cmake ../.. "-DBUILD_SCENARIO_VIEWER=OFF" "-DBUILD_TESTING=ON" "-DCMAKE_BUILD_TYPE=Debug" "-DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake"
+    cmake --build . --target $TARGET
     ./tests/$TARGET
 popd
