1b3e6253902153e8f96ec50223546bedc751798f
Eoin Flanagan <flanagep@tcd.ie>
2021-05-02 22:03:11 +0100

    Added benchmarks for the other update light functions

--- tests/benchmarks/main.cpp
+++ tests/benchmarks/main.cpp
@@ -3,5 +3,7 @@
 #include <benchmark/benchmark.h>
 
 BENCHMARK(ADD_LIGHT)->Unit(benchmark::kMillisecond)->MinTime(5);
+BENCHMARK(BROADEN_LIGHT)->Unit(benchmark::kMillisecond)->MinTime(5);
 BENCHMARK(MOVE_LIGHT)->Unit(benchmark::kMillisecond)->MinTime(5);
+BENCHMARK(ROTATE_LIGHT)->Unit(benchmark::kMillisecond)->MinTime(5);
 BENCHMARK_MAIN();
--- tests/benchmarks/updateLight.cpp
+++ tests/benchmarks/updateLight.cpp
@@ -11,9 +11,32 @@ static void MOVE_LIGHT(benchmark::State &state) {
 	LightmapManager lightmapManager(updateVars::TILE_SIZE, CastingAlgorithm::BOUND_RAY_CAST,
 									updateVars::CHUNK_SIZE);
 	auto lightId = lightmapManager.addLight(30, 30, glm::pi<float>(), updateVars::TILE_SIZE * 8);
-	auto light = lightmapManager.getLightsMap()[lightId].get();
+	auto &light = *lightmapManager.getLightsMap()[lightId];
 	for (auto _ : state) {
-		lightmapManager.updateLightPosition(lightId, light->x + 1, light->y);
+		lightmapManager.updateLightPosition(lightId, light.x + 1, light.y + 1);
+		lightmapManager.update();
+	}
+}
+
+static void ROTATE_LIGHT(benchmark::State &state) {
+	LightmapManager lightmapManager(updateVars::TILE_SIZE, CastingAlgorithm::BOUND_RAY_CAST,
+									updateVars::CHUNK_SIZE);
+	auto lightId = lightmapManager.addLight(30, 30, glm::pi<float>(), updateVars::TILE_SIZE * 8);
+	auto &light = *lightmapManager.getLightsMap()[lightId];
+	for (auto _ : state) {
+		lightmapManager.updateLightDirection(lightId,
+											 glm::vec2(-light.direction.y, light.direction.x));
+		lightmapManager.update();
+	}
+}
+
+static void BROADEN_LIGHT(benchmark::State &state) {
+	LightmapManager lightmapManager(updateVars::TILE_SIZE, CastingAlgorithm::BOUND_RAY_CAST,
+									updateVars::CHUNK_SIZE);
+	auto lightId = lightmapManager.addLight(30, 30, glm::pi<float>(), updateVars::TILE_SIZE * 8);
+	auto &light = *lightmapManager.getLightsMap()[lightId];
+	for (auto _ : state) {
+		lightmapManager.updateLightSpan(lightId, light.span + 0.3f);
 		lightmapManager.update();
 	}
 }
